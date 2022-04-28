77b57f1c00eac9ab556e278f8302d267ed860557
Zhengxu Xia <kevinxzx95@gmail.com>
2022-04-28 17:59:39 -0500

    Add emulation plot feature.

--- src/plot_scripts/plot_time_series.py
+++ src/plot_scripts/plot_time_series.py
@@ -1,10 +1,12 @@
 import argparse
+import csv
 import os
 from typing import Union
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 from simulator.network_simulator.constants import BITS_PER_BYTE, BYTES_PER_PACKET
@@ -29,6 +31,7 @@ def parse_args():
 
 def plot(trace: Union[Trace, None], log_file: str, save_dir: str, cc: str):
     df = pd.read_csv(log_file)
+    assert isinstance(df, pd.DataFrame)
     fig, axes = plt.subplots(6, 1, figsize=(12, 10))
     axes[0].set_title(cc)
     axes[0].plot(df['timestamp'], df['recv_rate'] / 1e6, 'o-', ms=2,
@@ -108,7 +111,7 @@ def plot(trace: Union[Trace, None], log_file: str, save_dir: str, cc: str):
     # axes[5].set_xlim(0, )
     plt.tight_layout()
     if save_dir is not None:
-        plt.savefig(os.path.join(save_dir, "{}_time_series.jpg".format(cc)))
+        fig.savefig(os.path.join(save_dir, "{}_time_series.jpg".format(cc)))
     plt.close()
 
     # if log_idx == 0:
@@ -130,6 +133,98 @@ def plot(trace: Union[Trace, None], log_file: str, save_dir: str, cc: str):
     #     bw_range = max(trace.bandwidths) - min(trace.bandwidths)
     #
     #     print("{},{},{},{},{},{}".format(bw_avg, bw_std, T_s_bw, change_cnt, bw_range, rtt_avg))
+
+
+def parse_aurora_emulation_log(log_file: str):
+    """Parse aurora emulation MI log."""
+    timestamps = []
+    recv_rates = []
+    send_rates = []
+    latencies = []
+    loss_rates = []
+    rewards = []
+    actions = []
+    send_start_times = []
+    send_end_times = []
+
+    with open(log_file, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            timestamps.append(float(row['timestamp']))
+            recv_rates.append(float(row['recv_rate']))
+            send_rates.append(float(row['send_rate']))
+            latencies.append(float(row['latency']))
+            loss_rates.append(float(row['loss']))
+            rewards.append(float(row['reward']))
+            actions.append(float(row['action']))
+            send_start_times.append(float(row['send_start_time']))
+            send_end_times.append(float(row['send_end_time']))
+
+    timestamps = np.array(timestamps)
+    recv_rates = np.array(recv_rates) / 1e6
+    send_rates = np.array(send_rates) / 1e6
+    latencies = np.array(latencies) * 1000
+    loss_rates = np.array(loss_rates)
+    rewards = np.array(rewards)
+    actions = np.array(actions)
+    send_start_times = np.array(send_start_times)
+    send_end_times = np.array(send_end_times)
+
+    return timestamps, recv_rates, send_rates, latencies, loss_rates, \
+        rewards, actions, send_start_times, send_end_times
+
+
+def plot_aurora_emulation_time_series(log_file: str, save_dir: str):
+    """Plot aurora MI level log from emulation/real world exp."""
+
+    timestamps, recv_rates, send_rates, latencies, loss_rates, rewards, \
+    actions, send_start_times, send_end_times = parse_aurora_emulation_log(log_file)
+
+    # df = pd.read_csv('test_aurora/aurora_emulation_log.csv')
+    fig, axes = plt.subplots(5, 1, figsize=(10, 10))
+    axes[0].plot(timestamps, recv_rates,
+                 label="Throughput avg {:.3f}Mbps".format(np.mean(recv_rates)))
+    axes[0].plot(timestamps, send_rates,
+                 label="Send rate avg {:.3f}Mbps".format(np.mean(send_rates)))
+    # axes[0].plot(np.arange(35), np.ones_like(
+    #     np.arange(35)) * 2, label='Link bandwidth')
+    axes[0].set_xlabel('Time(s)')
+    axes[0].set_ylabel('Mbps')
+    axes[0].legend()
+    # axes[0].set_ylim(0,  10)
+    axes[0].set_xlim(0, )
+
+    axes[1].plot(timestamps, latencies,
+                 label='RTT avg {:.3f}ms'.format(np.mean(latencies)))
+    axes[1].set_xlabel('Time(s)')
+    axes[1].set_ylabel('Latency(ms)')
+    axes[1].legend()
+    # axes[1].set_ylim(0, )
+    axes[1].set_xlim(0, )
+
+    axes[2].plot(timestamps, loss_rates,
+                 label='Loss avg {:.3f}'.format(np.mean(loss_rates)))
+    axes[2].set_xlabel('Time(s)')
+    axes[2].set_ylabel('Loss')
+    axes[2].legend()
+    axes[2].set_xlim(0, )
+    axes[2].set_ylim(0, 1)
+
+    axes[3].plot(timestamps, rewards,
+                 label='Reward avg {:.3f}'.format(np.mean(rewards)))
+    axes[3].set_xlabel('Time(s)')
+    axes[3].set_ylabel('Reward')
+    axes[3].legend()
+    axes[3].set_xlim(0, )
+
+    axes[4].plot(timestamps, actions, label='Action avg {:.3f}'.format(np.mean(actions)))
+    axes[4].set_xlabel('Time(s)')
+    axes[4].set_ylabel('Action')
+    axes[4].legend()
+    axes[4].set_xlim(0, )
+
+    plt.tight_layout()
+    fig.savefig(os.path.join(save_dir, "aurora_emulation.png"))
 
 
 def main():

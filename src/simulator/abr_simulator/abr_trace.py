Fix udr ranges to avoid infinite loop in env download time calculation.

src/simulator/abr_simulator/abr_trace.py
@@ -126,25 +126,25 @@ def generate_trace_from_ranges(bw_change_interval_range, min_bw_range,
             buffer_thresh_range[0] <= buffer_thresh_range[1]
 
     if bw_change_interval_range[0] == bw_change_interval_range[1]:
-        bw_change_interval = round(bw_change_interval_range[0])
+        bw_change_interval = bw_change_interval_range[0]
     else:
-        bw_change_interval = np.random.randint(
+        bw_change_interval = np.random.uniform(
             bw_change_interval_range[0], bw_change_interval_range[1])
 
     if min_bw_range[0] == min_bw_range[1]:
-        min_bw = round(min_bw_range[0])
+        min_bw = min_bw_range[0]
     else:
-        min_bw = np.random.randint(min_bw_range[0], min_bw_range[1])
-    max_bw = round(np.exp(np.random.uniform(np.log(max_bw_range[0]), np.log(max_bw_range[1]))))
+        min_bw = np.random.uniform(min_bw_range[0], min_bw_range[1])
+    max_bw = np.exp(np.random.uniform(np.log(max_bw_range[0]), np.log(max_bw_range[1])))
 
     if link_rtt_range[0] == link_rtt_range[1]:
-        link_rtt = round(link_rtt_range[0])
+        link_rtt = link_rtt_range[0]
     else:
-        link_rtt = np.random.randint(link_rtt_range[0], link_rtt_range[1])
+        link_rtt = np.random.uniform(link_rtt_range[0], link_rtt_range[1])
     if buffer_thresh_range[0] == buffer_thresh_range[1]:
-        buffer_thresh = round(buffer_thresh_range[0])
+        buffer_thresh = buffer_thresh_range[0]
     else:
-        buffer_thresh = np.random.randint(buffer_thresh_range[0], buffer_thresh_range[1])
+        buffer_thresh = np.random.uniform(buffer_thresh_range[0], buffer_thresh_range[1])
     return generate_trace(bw_change_interval, duration, min_bw, max_bw, link_rtt,
                           buffer_thresh)
 

src/simulator/abr_simulator/pensieve/pensieve.py
@@ -305,11 +305,12 @@ class Pensieve:
         with tf.Session() as sess, \
                 open(os.path.join(save_dir, 'log_train'), 'w', 1) as log_central_file, \
                 open(os.path.join(save_dir, 'log_val'), 'w', 1) as val_log_file:
-            log_writer = csv.writer(log_central_file, delimiter='\t')
+            log_writer = csv.writer(log_central_file, delimiter='\t', lineterminator='\n')
             log_writer.writerow(['epoch', 'loss', 'avg_reward', 'avg_entropy'])
-            val_log_file.write("\t".join(
+            val_log_writer = csv.writer(val_log_file, delimiter='\t', lineterminator='\n')
+            val_log_writer.writerow(
                 ['epoch', 'rewards_min', 'rewards_5per', 'rewards_mean',
-                 'rewards_median', 'rewards_95per', 'rewards_max\n']))
+                 'rewards_median', 'rewards_95per', 'rewards_max'])
 
             actor = a3c.ActorNetwork(sess,
                                      state_dim=[self.s_info, self.s_len],
@@ -426,11 +427,12 @@ class Pensieve:
                         actor, trace, video_size_file_dir=video_size_file_dir,
                         save_dir=save_dir) for trace in val_traces]
                     val_mean_reward = np.mean(val_rewards)
-                    # print(val_rewards)
-                    print(epoch, val_mean_reward)
-                    # test_mean_reward = testing(
-                    #     args, epoch, actor, val_log_file, args.val_trace_dir,
-                    #     os.path.join(args.summary_dir, 'test_results'))
+
+                    val_log_writer.writerow(
+                            [epoch, np.min(val_rewards),
+                             np.percentile(val_rewards, 5), np.mean(val_rewards),
+                             np.median(val_rewards), np.percentile(val_rewards, 95),
+                             np.max(val_rewards)])
                     val_epochs.append(epoch)
                     val_mean_rewards.append(val_mean_reward)
                     average_rewards.append(np.sum(avg_reward))
@@ -504,8 +506,9 @@ def agent(train_seq_len: int, s_info: int, s_len: int, a_dim: int,
     net_env = Environment(trace_scheduler, VIDEO_CHUNK_LEN / MILLISECONDS_IN_SECOND,
                           video_size_file_dir=video_size_file_dir,
                           random_seed=agent_id)
-    with tf.compat.v1.Session() as sess, open(os.path.join(
-            save_dir, f'log_agent_{agent_id}'), 'w') as log_file:
+    with tf.compat.v1.Session() as sess:
+        # , open(os.path.join(
+        #     save_dir, f'log_agent_{agent_id}'), 'w') as log_file:
 
         # log_file.write('\t'.join(['time_stamp', 'bit_rate', 'buffer_size',
         #                'rebuffer', 'video_chunk_size', 'delay', 'reward',
@@ -629,7 +632,7 @@ def agent(train_seq_len: int, s_info: int, s_len: int, a_dim: int,
                 del entropy_record[:]
 
                 # so that in the log we know where video ends
-                log_file.write('\n')
+                # log_file.write('\n')
 
             # store the state and action into batches
             if end_of_video: